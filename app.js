63eafe9916b28889e0d3e6e890450decfe5e0659
TAMOHARINI R <tamoharini2005@gmail.com>
2026-06-05 08:49:21 +0530

    Add root route to confirm API is running

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const recipeRouter = require("./routes/recipeRoutes");
 const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Recipes API is running successfully");
+});
 
 app.use("/", recipeRouter);
 module.exports = app;
